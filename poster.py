5c4aed0567df198b22dd55ca375d37dc7e28ad7e
William <wkehr@rednovalabs.net>
2013-01-24 23:23:49 -0600

    Clean up some debug code

--- poster.py
+++ poster.py
@@ -27,55 +27,48 @@ class PosterCommand(sublime_plugin.WindowCommand):
 		key = self.items[picked][0]
 		data = self.json[key]
 		value = None
-		# try:
-		method = data.get("method") if data.get("method") is not None else "get"
-		body = data.get("data")
-		body = urllib.urlencode(body) if body else None
-		url = data.get("url")
-		url_parsed = urlparse(url)
-		header = data.get("header")
-		header = dict() if not header else header
-		path = url_parsed.path
-		query = url_parsed.query
-		if method.lower() == "get" and body is not None:
-			query = body if not query else "%s&%s" % (query, body)
-			body = None
-		if query:
-			path += "?%s" % query
-		if method.lower() == "post":
-			header = dict(header)
-			header["Content-type"] = "application/x-www-form-urlencoded"
-		
-		full_url = "%s://%s%s" % (url_parsed.scheme, url_parsed.netloc, query)
-		# req = urllib2.Request(full_url, data, header)
-		# response = urllib2.urlopen(req)
-		# contents = response.read()
+		try:
+			method = data.get("method") if data.get("method") is not None else "get"
+			body = data.get("data")
+			body = urllib.urlencode(body) if body else None
+			url = data.get("url")
+			url_parsed = urlparse(url)
+			header = data.get("header")
+			header = dict() if not header else header
+			path = url_parsed.path
+			query = url_parsed.query
+			if method.lower() == "get" and body is not None:
+				query = body if not query else "%s&%s" % (query, body)
+				body = None
+			if query:
+				path += "?%s" % query
+			if method.lower() == "post":
+				header = dict(header)
+				header["Content-type"] = "application/x-www-form-urlencoded"
+			
+			if url_parsed.scheme == "http":
+				request = httplib.HTTPConnection(url_parsed.netloc)
+				request.request(method.upper(), path, body, header)
+				response = request.getresponse()
+				contents = response.read()
+			elif hasattr(httplib, "HTTPSConnection"):
+				request = httplib.HTTPSConnection(url_parsed.netloc)
+				request.request(method.upper(), path, body, header)
+				response = request.getresponse()
+				contents = response.read()
+			else:
+				full_url = "%s://%s%s%s" % (url_parsed.scheme, url_parsed.netloc, path, query)
+				contents = curl.post(full_url, body, header)
 
-		if url_parsed.scheme == "http":
-			request = httplib.HTTPConnection(url_parsed.netloc)
-			request.request(method.upper(), path, body, header)
-			response = request.getresponse()
-			contents = response.read()
-		elif hasattr(httplib, "HTTPSConnection"):
-			request = httplib.HTTPSConnection(url_parsed.netloc)
-			request.request(method.upper(), path, body, header)
-			response = request.getresponse()
-			contents = response.read()
-			# sublime.status_message("HTTPS currently not supported")
-			# return
-		else:
-			full_url = "%s://%s%s%s" % (url_parsed.scheme, url_parsed.netloc, path, query)
-			contents = curl.post(full_url, body, header)
+			v = self.window.new_file()
+			v.set_name("Poster Results")
+			v.set_scratch(True)
+			edit = v.begin_edit()
+			v.insert(edit, 0, contents)
+			v.end_edit(edit)
 
-		v = self.window.new_file()
-		v.set_name("Poster Results")
-		v.set_scratch(True)
-		edit = v.begin_edit()
-		v.insert(edit, 0, contents)
-		v.end_edit(edit)
-
-		# except:
-		# 	sublime.status_message("HTTP Request failed!")
+		except:
+			sublime.status_message("HTTP Request failed!")
 
 	def run(self):
 		try:
